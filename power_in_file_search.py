8591e8138832711ce1a0a1201985bcc2d7a738be
thayashi <ihsayaht82tco@gmail.com>
2023-09-19 08:04:23 +0900

    improve power_in_file_search -- added around_content.

--- power_in_file_search.py
+++ power_in_file_search.py
@@ -8,27 +8,35 @@ class PowerInFileSearch:
         self.file_list = file_list
         self.df = None
 
-    def _search_file(self, file_path, keywords, flg_casesensitive):
+    def _search_file(self, file_path, keywords, flg_casesensitive, nrows):
         hits = []
         with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-            for line_num, line in enumerate(f, 1):
+            lines = f.readlines()
+            for line_num, line in enumerate(lines, 1):
                 search_line = line if flg_casesensitive else line.lower()
                 for keyword in keywords:
                     search_keyword = keyword if flg_casesensitive else keyword.lower()
                     if search_keyword in search_line:
                         dir_path, file_name = os.path.split(file_path)
-                        hits.append((file_path, dir_path, file_name, line_num, line.strip()))
+                        
+                        # 前後の行を含めた内容を取得
+                        start_index = max(0, line_num - nrows - 1)
+                        end_index = min(len(lines), line_num + nrows - 1)
+                        around_lines = lines[start_index:end_index]
+                        around_content = ''.join(around_lines).strip()
+                        
+                        hits.append((file_path, dir_path, file_name, line_num, line.strip(), around_content))
                         break
         return hits
 
-    def search(self, keywords, flg_casesensitive=True):
+    def search(self, keywords, flg_casesensitive=True, nrows=3):
         results = []
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = {executor.submit(self._search_file, file_path, keywords, flg_casesensitive): file_path for file_path in self.file_list}
+            futures = {executor.submit(self._search_file, file_path, keywords, flg_casesensitive, nrows): file_path for file_path in self.file_list}
             for future in concurrent.futures.as_completed(futures):
                 results.extend(future.result())
 
-        self.df = pd.DataFrame(results, columns=["file_path", "dir_path", "file_name", "line_number", "line_content"])
+        self.df = pd.DataFrame(results, columns=["file_path", "dir_path", "file_name", "line_number", "line_content", "around_content"])
         self.df["link"] = "<a href='" + self.df["file_path"] + "'>link</a>"
 
     def show(self):
